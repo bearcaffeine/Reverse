76f2d1de9a443497b2b14ec181dc4986374abc39
Brad <bjcost@gmail.com>
2021-08-02 15:49:25 -0400

    Updates for Reverse site

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# JSSiteTemplate
-Site Template for JS projects
+# REVERSE: A Coding Project
+There and back again. A javascript coding exercise using arrays. Write a program that displays a string in reverse.
+- CSS and Boostrap Layout
+- Javascript Fundamentals
+- Javascript Loops
+- Javascript Functions
+- Javascript DOM Manipulation
+- Javascirpt If/Then/Else
+- Javascript Boolean Logic
--- js/site.js
+++ js/site.js
@@ -1,4 +1,33 @@
-//Call Hello World
-function HelloWorld(){
-    alert("hello world");
+//Get the string from the page
+function getValue(){
+    
+    document.getElementById("alert").classList.add("invisible");
+    
+    let userString = document.getElementById("userString").value;
+    
+    let rev = reverseString(userString);
+    
+    displayString(rev);
+}
+
+//Reverse the string
+function reverseString(userString){
+    let revString = []; 
+
+    //reverse a string using a for loop
+    for (let index = userString.length-1; index >= 0; index--) {
+        revString += userString[index];
+    }
+
+    return revString;
+}
+
+//Display the message with reverse string
+function displayString(revString){
+    //write to the page
+    document.getElementById("message").innerHTML = `Your string reversed is: ${revString}`;
+    
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
+
 }
